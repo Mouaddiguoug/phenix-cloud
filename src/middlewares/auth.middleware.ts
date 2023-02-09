bbbffe0a77329118716c550cead51fd341b92b8c
unknown <mouaddigoug12@gmail.com>
2023-02-09 12:35:20 +0100

    second*

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 import { SECRET_KEY } from '@config';
 import { HttpException } from '@exceptions/HttpException';
@@ -6,23 +6,17 @@ import { DataStoredInToken, RequestWithUser } from '@interfaces/auth.interface';
 import userModel from '@models/users.model';
 import { con } from '../app';
 
-const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const Authorization = req.cookies['Authorization'] || (req.header('Authorization') ? req.header('Authorization').split('Bearer ')[1] : null);
-
+    const Authorization = (req.header('auth') ? req.header('auth').split('Bearer ')[1] : null);
     if (Authorization) {
       const secretKey: string = SECRET_KEY;
       const verificationResponse = (await verify(Authorization, secretKey)) as DataStoredInToken;
-      console.log(verificationResponse);
-
       const { email } = verificationResponse;
-      const expiresIn: string = '10 hours';
-
       const [result] = await con.query(`select * from admin where email = '${email}'`);
-
+      
       if (result.length > 0)
       {
-        req.user = result;
         next();
       } else {
         next(new HttpException(401, 'Wrong authentication token'));
