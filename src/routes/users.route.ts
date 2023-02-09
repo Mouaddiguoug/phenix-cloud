3459618ec3fafbfa546ef97522d3b9a00abfeca8
unknown <mouaddigoug12@gmail.com>
2023-02-09 12:47:55 +0100

    final

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -16,16 +16,16 @@ class UsersRoute implements Routes {
 
   private initializeRoutes() {
     this.router.get(`${this.path}`, authMiddleware, this.usersController.getUsers);
-    this.router.get(`${this.path}/payments/:id`, this.usersController.getPayments);
-    this.router.post(`${this.path}/payments/`, this.usersController.createPayment);
-    this.router.get(`${this.path}/subscriptions/:id`, this.usersController.getSubscription);
-    this.router.get(`${this.path}/:id`, this.usersController.getUserById);
+    this.router.get(`${this.path}/payments/:id`, authMiddleware, this.usersController.getPayments);
+    this.router.post(`${this.path}/payments/`, authMiddleware, this.usersController.createPayment);
+    this.router.get(`${this.path}/subscriptions/:id`, authMiddleware, this.usersController.getSubscription);
+    this.router.get(`${this.path}/:id`, authMiddleware, this.usersController.getUserById);
     this.router.get(`${this.path}/mandates/:id`, authMiddleware, this.usersController.getMandates);
-    this.router.post(`${this.path}/mandates/:id`, this.usersController.createMandate);
-    this.router.post(`${this.path}/subscriptions/`, this.usersController.createSubscription);
-    this.router.post(`${this.path}`, this.usersController.createCustomer);
-    this.router.put(`${this.path}/:id`, this.usersController.updateUser);
-    this.router.delete(`${this.path}/:id`, this.usersController.deleteCustomer);
+    this.router.post(`${this.path}/mandates/:id`, authMiddleware, this.usersController.createMandate);
+    this.router.post(`${this.path}/subscriptions/`, authMiddleware, this.usersController.createSubscription);
+    this.router.post(`${this.path}`, authMiddleware, this.usersController.createCustomer);
+    this.router.put(`${this.path}/:id`, authMiddleware, this.usersController.updateUser);
+    this.router.delete(`${this.path}/:id`, authMiddleware, this.usersController.deleteCustomer);
   }
 }
 
